dccb90e5f3f9c7d067e86520669f70ea09f8f77c
ThePrimeagen <the.primeagen@aol.com>
2026-06-30 13:30:36 -0700

    got it to behave like a simple file servr

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
 func main() {
 	mux := http.NewServeMux()
+	fileHandler := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileHandler)
+
 	server := http.Server{}
 	server.Addr = ":8080"
 	server.Handler = mux
