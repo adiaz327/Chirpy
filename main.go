d382a3c4e5da4954e4fea74205cf57df9fdf71e8
ThePrimeagen <the.primeagen@aol.com>
2026-06-30 14:38:43 -0700

    got it to do some header altering and path manipulation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"io"
 )
 
 type apiHandler struct{}
@@ -11,7 +12,12 @@ func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 func main() {
 	mux := http.NewServeMux()
 	fileHandler := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", fileHandler))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){
+		req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		io.WriteString(w, "200 OK")
+	})
 
 	server := http.Server{}
 	server.Addr = ":8080"
