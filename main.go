faa270950b6ce79f8ff363c22bf91f2c0571da58
ThePrimeagen <the.primeagen@aol.com>
2026-06-30 16:05:31 -0700

    got it working

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"io"
+	"net/http"
+	"sync/atomic"
 )
 
 type apiHandler struct{}
 
 func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) getFileserverHits() int32 {
+	return cfg.fileserverHits.Load()
+}
+
+func (cfg *apiConfig) resetFileserverHits() {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
 	fileHandler := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileHandler))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileHandler)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		req.Header.Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		io.WriteString(w, "200 OK")
+	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		io.WriteString(w, fmt.Sprintf("Hits: %d", cfg.getFileserverHits()))
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+		cfg.resetFileserverHits()
+		w.WriteHeader(200)
 	})
 
 	server := http.Server{}
